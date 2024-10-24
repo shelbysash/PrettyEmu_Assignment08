e26654952380b58999677068c79fc770b147e29c
Shelby Sash <shelbyksash@gmail.com>
2024-10-24 10:05:09 -0400

    Added main.py comments. Testing if I can push

--- PrettyEmu_Assignment08/package1/main.py
+++ PrettyEmu_Assignment08/package1/main.py
@@ -1,3 +1,19 @@
-#main.py
-#shelby py
+# Name: Shelby Sash
+# email: sashsk@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: October 31,2024
+# Course #/Section: IS4010/ 001
+# Semester/Year: Fall 2024
+# Brief Description of the assignment: Collaborate with a group to practice OOP principles and architecture
+# Brief Description of what this module does: Entry point for the project  
+# Citations: 
+# Anything else that's relevant: N/A
 
+#main.py
+#shelby 
+
+"""
+instantiates at least one object of each type,
+invokes the dunder and non-dunder methods in the object, and
+Prints sufficient information to demonstrate the functionality of all the classes.
+"""
