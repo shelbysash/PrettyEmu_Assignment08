e00fae92758c8124f2474df33937d15747dad886
Sidney Huschart <huschash@mail.uc.edu>
2024-10-30 13:47:47 -0400

    test 2

--- PrettyEmu_Assignment08/package2/Sidney.py
+++ PrettyEmu_Assignment08/package2/Sidney.py
@@ -1,3 +1,16 @@
-#Sidney
+#Sidney.py
 
-does this work
+# Name: Sidney Huschart
+# email:  huschash@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date:   October 31, 2024
+# Course #/Section:   IS4010-001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment:  working as a team to collaborate using github to display our knowlegd of classes. 
+
+# Brief Description of what this module does:   demonstrates our knowledge of classes and how to use them. 
+# Citations:
+# Anything else that's relevant:
+
+class ChristmasCelebration(object):
+    
