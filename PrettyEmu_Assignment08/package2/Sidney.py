0ab0e9e5a15547d421c99323ad83bb5584fce216
Sidney Huschart <huschash@mail.uc.edu>
2024-10-30 14:20:53 -0400

    implementing the christmas cookie module.

--- PrettyEmu_Assignment08/package2/Sidney.py
+++ PrettyEmu_Assignment08/package2/Sidney.py
@@ -14,61 +14,61 @@
 
 class ChristmasCookies(object):
     """
-        
+    Model a Christmas cookie that can be made during December. 
     """
-    def __init__(self, type):
+    def __init__(self, type):  #__init__
         """
-        Constructor (duh)
+        Constructor
         @param type String: The type of cookie
         
         """
         self.__type = type
 
-    def get_type(self):
+    def get_type(self): #getter
         """
-        @return String: The  type of the current object
+        @return String: The cookie type of the current object
         """
         return self.__type
     
-    def set_type(self, type):
+    def set_type(self, type): #setter
         """
-        Assign a value to the vehicle type of the current object
-        @param type String: The vehicle type to be assigned.
+        Assign a value to the cookie type of the current object
+        @param type String: The cookie type to be assigned.
         """
         self.__type = type
         
-    def print_type(self):
+    def print_type(self):  #class that does something-- this will print something
         """
-        Print the vehicle type of the current object
+        Print the cookie type of the current object
         """
         print(self.__type)
         
-    def __str__(self):
+    def __str__(self):  #__str__
         """
         @return String: A human-readable basic representation of the current object. 
         Useful for debugging, documentation, etc.
         """
         return "type: " + self.__type
 
-    def __repr__(self):
+    def __repr__(self): #__repr__ 
         """
         @return String: A string containing code that can be executed to create a copy of the current object
         """
-        return f"Vehicle('{self.__type}')"
+        return f"Cookie('{self.__type}')"
     
 if __name__ == "__main__":
-    print("Vehicle class test logic...")
-    car = Vehicle("Corvette")
-    print("Type attribute of Vehicle object:", car.get_type())
+    print("Christmas cookie class test logic...")
+    cookies = ChristmasCookies("Sugar Cookie") 
+    print("Type attribute of Cookie object:", cookies.get_type())
     print("Changing the type of the object...")
-    car.set_type("GTO")
-    print("Type attribute of Vehicle object:", car.get_type())
+    cookies.set_type("Frosted Sugtar Cookie")
+    print("Type attribute of the Christmas Cookies object:", cookies.get_type())
 
     print("\n\nTesting the repr method...")
 
-    print("From __repr__():", car.__repr__())
-    carCopy = eval(car.__repr__())
-    print("Copied car:", carCopy.__str__())
+    print("From __repr__():", cookies.__repr__())
+    cookiesCopy = eval(cookies.__repr__())
+    print("Copied car:", cookiesCopy.__str__())
 
-    print("Type attribute of original Vehicle object:", car.get_type())
-    print("Type attribute of copied Vehicle object:", carCopy.get_type())
+    print("Type attribute of original Christmas Cookies object:", cookies.get_type())
+    print("Type attribute of copied Christmas Cookies object:", cookiesCopy.get_type())
